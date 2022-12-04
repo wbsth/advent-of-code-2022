a8567472adc3df7e5169d5b66c2e5d15222a868a
Michal <18263943+wbsth@users.noreply.github.com>
2022-12-04 14:29:52 +0100

    Day 1 completed

---
+++ AdventOfCode2022/day1/Program.cs
@@ -0,0 +1,29 @@
+﻿string filePath = @"./input.txt";
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine("no file found");
+    return;
+}
+
+var linesEnumerable = File.ReadLines(filePath);
+
+int temp = 0;
+List<int> caloriesList = new List<int>();
+
+foreach(var line in linesEnumerable)
+{
+    if(line.Length == 0)
+    {
+        caloriesList.Add(temp);
+        temp = 0;
+    }
+    else
+    {
+        temp += int.Parse(line);
+    }
+}
+
+Console.WriteLine($"Max calories count:{caloriesList.Max()}");
+
+Console.WriteLine($"Sum of calories for top 3 elves: {caloriesList.OrderByDescending(x=>x).Take(3).Sum()}");
