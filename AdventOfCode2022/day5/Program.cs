c585705eccebe40cda8c4292ffc69913380ebfd4
Michal <18263943+wbsth@users.noreply.github.com>
2022-12-06 22:32:17 +0100

    Day 5 first part

---
+++ AdventOfCode2022/day5/Program.cs
@@ -0,0 +1,69 @@
+﻿string filePath = @"./input.txt";
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine("no file found");
+    return;
+}
+
+var lines = File.ReadAllLines(filePath);
+
+int boxWidth = lines[0][1..].Where((x, i) => i % 4 == 0).Count();
+int boxHeight = 0;
+
+for (int i = 0; i < lines.Length; i++)
+{
+    if (lines[i].Length == 0)
+    {
+        boxHeight = i - 1;
+    }
+}
+
+var itemArray = new List<char>[boxWidth];
+for (int i = 0; i < boxWidth; i++)
+{
+    itemArray[i] = new List<char>();
+}
+
+for (var i = 0; i < boxHeight; i++)
+{
+    var line = lines[i];
+    var temp = line[1..].Where((x, i) => i % 4 == 0).ToList();
+    for (int j = 0; j < temp.Count; j++)
+    {
+        if(temp[j] != ' ')
+            itemArray[j].Add(temp[j]);
+    }
+
+}
+
+for (var i = boxHeight + 2; i < lines.Length; i++)
+{
+    var move = lines[i];
+    var splitted = move.Split(' ');
+    var qty = int.Parse(splitted[1]);
+    var from = int.Parse(splitted[3]);
+    var to = int.Parse(splitted[5]);
+    
+    // get items to move
+    var tmp = itemArray[from - 1].Take(qty).ToList();
+    
+    // add to new stack
+    foreach (var item in tmp)
+    {
+        itemArray[to - 1].Insert(0, item);
+    }
+    
+    // remove from src
+    itemArray[from - 1].RemoveRange(0,qty);
+    
+}
+
+var tempList = new List<char>();
+foreach (var list in itemArray)
+{
+    tempList.Add(list[0]);
+}
+
+var joinedString = new string(tempList.ToArray());
+Console.WriteLine($"Crates on top: {joinedString}");
